e21813e0c2eab6a5dc191f321a85a04d1d07afb4
irrum6 <irrum6@github.com>
2021-03-21 01:45:04 +0400

    wd rf

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react'
-// import './Card.module.css'
+import React from 'react'
 
 const card = (props) => {
     const data = props.cdata;
 
-    let { show, suit, symbol, color, value } = data;
-
-    // const GENERIC_SYMBOL = "♫"
-
-    // symbol = show ? symbol : GENERIC_SYMBOL;
-    // color = show ? color : 'black';
+    let { show, suit, value } = data;
     value = show ? value : '';
 
-    const cssColorClass = `card-${color}`;
-
     let backCssClass = show ? suit: "hidden_card";
-
     let boxClassName = `flex space-between column pxq pyq card-box ${backCssClass}`;
+
     return (
         <div className={boxClassName}>            
             <div className="flex row pth pbh card-body">
