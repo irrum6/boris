42ad009d7087da9ae699d5bd4489895fcf696bd6
irrum6 <irrum6@github.com>
2021-04-07 00:51:02 +0400

    rgb

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react"
 import Link from "next/link"
 
+let _rgb = "0";
+const setrgb = () => {
+    _rgb = _rgb == "0" ? "1" : "0";
+    // console.log(1);
+}
+
 const DisplayDate = (props) => {
 
     const [date, setDate] = useState(0);
+    // const [rgb, setRGB] = useState(props.rgb);
 
     useEffect(() => {
-        const tid = setInterval(() => { setDate(Date.now()) }, 1000);
+        const tid = setInterval(() => { setDate(Date.now()) }, 300);
         return () => clearInterval(tid);//return clearer
     });
 
@@ -25,17 +32,17 @@ const DisplayDate = (props) => {
     const s0 = Math.floor(s / 10);
     const s1 = s % 10;
 
-    if (props.rgb == "1") {
+    if (_rgb == "1") {
         return (<div className="flex row centered">
             <div>
-                <span className="color_red f4 bolder mxq">{h0}</span>
-                <span className="color_red f4 bolder">{h1}</span>
+                <span className="color_red bg_litegray f4 bolder mxq pxh">{h0}</span>
+                <span className="color_red bg_litegray f4 bolder pxh">{h1}</span>
                 <span className="f4 bolder">:</span>
-                <span className="color_green f4 bolder mxq">{m0}</span>
-                <span className="color_green f4 bolder">{m1}</span>
+                <span className="color_green bg_litegray f4 bolder mxq pxh">{m0}</span>
+                <span className="color_green bg_litegray f4 bolder pxh">{m1}</span>
                 <span className="f4 bolder">:</span>
-                <span className="color_blue f4 bolder mxq">{s0}</span>
-                <span className="color_blue f4 bolder">{s1}</span>
+                <span className="color_blue bg_litegray f4 bolder mxq pxh">{s0}</span>
+                <span className="color_blue bg_litegray f4 bolder pxh">{s1}</span>
             </div>
         </div>);
     }
@@ -58,6 +65,13 @@ export default function Home() {
     const date = new Date();
     return (
         <div>
+            <div className="flex stretch bg_darkgray myh pxq pyh">
+                <button className="f2 bolder bg-blend bowq bo_black bo-solid pointer" onClick={setrgb}>
+                    <span className="color_red">R</span>
+                    <span className="color_green">G</span>
+                    <span className="color_blue">B</span>
+                </button>
+            </div>
             <DisplayDate rgb="0" />
             {/* <DisplayDate/> */}
             <div>
