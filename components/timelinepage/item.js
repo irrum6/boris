e6a2e5bbfaac21e3ed40343b5952b925631ac08b
irakli mindorashvili <irrum6@github.com>
2022-11-08 23:09:03 +0400

    timeline update 2

--- components/timelinepage/item.js
+++ components/timelinepage/item.js
@@ -2,16 +2,39 @@ import React, { Component } from 'react';
 
 import styles from "./item.module.css"
 
-const {item_,content} = styles;
+const { item_, content } = styles;
 
+const monMap = {
+    "Jan": "1",
+    "Feb": "2",
+    "Mar": "3",
+    "Apr": "4",
+    "May": "5",
+    "Jun": "6",
+    "Jul": "7",
+    "Aug": "8",
+    "Sep": "9",
+    "Oct": "10",
+    "Nov": "11",
+    "Dec": "12"
+}
 class TimelineItem extends Component {
     render() {
-        let { message, date, fullhash, hash,files } = this.props.data;
-        let filestxt = files.map((e,i)=>(<div key={i}>{e}</div>))
+        let { message, date, fullhash, hash, files } = this.props.data;
+        let filestxt = files.map((e, i) => (<div key={i}>{e}</div>));
+        //date split
+        let ds = date.replace(",").split(" ");
+        let dd = ds[1];
+        let mm = monMap[ds[0]];
+        let yy = ds[2];
+        let formated = `${yy}-${mm}-${dd}`;
+
         return (
             <div className={item_}>
                 <div className={`${content} ph`}>
-                    <div className='ph'>{date}</div>
+                    <div className='ph'>
+                        <time dateTime={`${formated}`}>{date}</time>
+                    </div>
                     <div className='ph'>{message}</div>
                     <div className='ph'>Changes:{filestxt}</div>
                     <div className='ph bolder'>
--- pages/timeline.js
+++ pages/timeline.js
@@ -78,7 +78,7 @@ let timedata = [
     {
         branch: "main",
         message: " readme updated ",
-        date: "Oct 6, 2021",
+        date: "Oct 6 2021",
         fullhash: "3c819e57b5019230656fb7c6590e5ea8a49e2676",
         hash: "3c819e5",
         files: ["readme.md"]
@@ -86,7 +86,7 @@ let timedata = [
     {
         branch: "main",
         message: " move imports ",
-        date: "May 12, 2021 ",
+        date: "May 12 2021 ",
         fullhash: "d4d4e23a04cc13f1fca258b5f3ac0368f8d69b9f",
         hash: "d4d4e23",
         files: [" 7 changed files with 114 additions and 175 deletions. "]
@@ -94,7 +94,7 @@ let timedata = [
     {
         branch: "main",
         message: "port",
-        date: " Apr 7, 2021",
+        date: " Apr 7 2021",
         fullhash: "c758ed1bde6a0226ca697dc53e3ffe91bb8b6005",
         hash: "c758ed1",
         files: ["package.json"]
@@ -102,7 +102,7 @@ let timedata = [
     {
         branch: "main",
         message: "rgb",
-        date: " Apr 7, 2021",
+        date: " Apr 7 2021",
         fullhash: "42ad009d7087da9ae699d5bd4489895fcf696bd6",
         hash: "42ad009",
         files: ["7 changed files with 258 additions and 10 deletions. "]
@@ -110,70 +110,70 @@ let timedata = [
     {
         branch: "main",
         message: "clock",
-        date: " Apr 6, 2021",
+        date: " Apr 6 2021",
         fullhash: "8e7043b58ce0cfd7d1d36551baa5fd1d399d67fe",
         hash: "8e7043b",
         files: ["11 changed files with 306 additions and 684 deletions. "]
     },
     {
         message: "rf2",
-        date: " Apr 1, 2021",
+        date: " Apr 1 2021",
         fullhash: "f356d8cf302908d2b515074797c61ba3cb23a79c",
         hash: "f356d8c",
         files: ["package.json"]
     },
     {
         message: "rf2",
-        date: " Apr 1, 2021",
+        date: " Apr 1 2021",
         fullhash: "453c4b59b34799bfa1310f277cb2f1e899434b1e",
         hash: "453c4b5",
         files: [" 46 changed files with 1,335 additions and 386 deletions."]
     },
     {
         message: " worked out  ",
-        date: " Apr 1, 2021",
+        date: " Apr 1 2021",
         fullhash: "071805badcda5492c77deef00c6801da91b7bce9",
         hash: "071805b",
         files: [" 3 changed files with 665 additions and 1 deletion.", ".gitignore", "styles/colors.css", "styles/common.css"]
     },
     {
         message: " worked out  ",
-        date: " Apr 1, 2021",
+        date: " Apr 1 2021",
         fullhash: "8c49c875ab45e4a1a1bd049428a0e6ec5b150ad2",
         hash: "8c49c87",
         files: ["38 changed files with 10 additions and 674 deletions."]
     },
     {
         message: "f4 ",
-        date: "Mar 24, 2021",
+        date: "Mar 24 2021",
         fullhash: "e571a9de6682f4c3393a7855a83617d02f2dccb3",
         hash: "e571a9d",
         files: ["3 changed files with 35 additions and 35 deletions. ", "pages/App.js", "components/Board.js", "components/listwindow.js"]
     },
     {
         message: "wd rf ",
-        date: "Mar 21, 2021",
+        date: "Mar 21 2021",
         fullhash: "e21813e0c2eab6a5dc191f321a85a04d1d07afb4",
         hash: "e21813e",
         files: ["12 changed files with 313 additions and 3,881 deletions. "]
     },
     {
         message: 'nodevers',
-        date: "Mar 21, 2021",
+        date: "Mar 21 2021",
         fullhash: "e8d879d2c30a0ee4b39b2bfcd9c92e8c1006de6a",
         hash: "e8d879d",
         files: ["package.json"]
     },
     {
         message: 'initial',
-        date: "Mar 20, 2021",
+        date: "Mar 20 2021",
         fullhash: "2a860b7d400a875beb1b11896dacc3bd4c377cb5",
         hash: "2a860b7",
         files: [" 51 changed files with 8,176 additions and 0 deletions."]
     },
     {
         message: 'first commit',
-        date: "Mar 20, 2021",
+        date: "Mar 20 2021",
         fullhash: "3350c1486b40e9daa42b759e7f839e4fc1566f70",
         hash: "3350c14",
         files: ["README.md"]
