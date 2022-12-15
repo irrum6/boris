08397382c98d7e343fcd2c867643ba235dcfe451
irakli mindorashvili <irrum6@github.com>
2022-12-15 20:12:38 +0400

    better card faces

--- components/cardspage/card/index.js
+++ components/cardspage/card/index.js
@@ -8,13 +8,16 @@ const card = (props) => {
     let { show, suit, value } = data;
     value = show ? value : '';
 
-    let backCssClass = show ? styles[suit]: styles["hidden_card"];
+    let backCssClass = show ? styles[suit] : styles["hidden_card"];
+    if (true == show && "clubs" == suit) {
+        backCssClass = `${suit}_${value}`;
+    }
     let boxClassName = `flex space-between column pxq pyq ${styles["card-box"]} ${backCssClass}`;
     return (
-        <div className={boxClassName}>            
+        <div className={boxClassName}>
             <div className={`${styles["card-body"]} flex row pth pbh`}>
-                <span className="f4">{value}</span>
-            </div>            
+                <span className="f4">{("clubs" == suit)?"":value}</span>
+            </div>
         </div>
     );
 }
