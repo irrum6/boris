fa19d4b4ca7b3e03d3794a56dc80f433a0df416a
irakli mindorashvili <irrum6@github.com>
2022-12-21 00:19:21 +0400

    spades drawn 1

--- components/cardspage/card/index.js
+++ components/cardspage/card/index.js
@@ -9,14 +9,16 @@ const card = (props) => {
     value = show ? value : '';
 
     let backCssClass = show ? styles[suit] : styles["hidden_card"];
-    if (true == show && "clubs" == suit) {
+    let isDrawnSuit=(true == show && ("clubs" == suit ||"spades" == suit));
+
+    if (isDrawnSuit) {
         backCssClass = `${suit}_${value}`;
     }
     let boxClassName = `flex space-between column pxq pyq ${styles["card-box"]} ${backCssClass}`;
     return (
         <div className={boxClassName}>
             <div className={`${styles["card-body"]} flex row pth pbh`}>
-                <span className="f4">{("clubs" == suit)?"":value}</span>
+                <span className="f4">{(isDrawnSuit)?"":value}</span>
             </div>
         </div>
     );
