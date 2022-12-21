553033aae2d5f94a60ea54ed0d962219564e3153
irakli mindorashvili <irrum6@github.com>
2022-12-21 23:45:30 +0400

    rf

--- components/memorypage/Navbar.js
+++ components/memorypage/Navbar.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import styles from './Navbar.module.css';
+import React from 'react'
+import styled from "styled-components"
 
+const Box =styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 700px) {
+        flex-direction: column;
+    }
+`
 
 const Navbar = (props) => (
-    <div className={`${styles.navbar} bg_navbar_blue color_white ph`}>
+    <Box className="bg_navbar_blue color_white ph">
         <a className="color_white bolder" href="/">&lt;&lt; უკან</a>
         <div className='ph'>
             <button className="bg_green ph f1h" onClick={props.onNewGame}>New Game</button>
         </div>
-    </div>
+    </Box>
 );
 
 export default Navbar;
